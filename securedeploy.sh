5007919f26d47a1f5076b8b96a3b46d042f2936c
Ryan Lonergan <Ryan.tlonergan@gmail.com>
2020-03-16 18:32:24 -0400

    Forgot to save securedeploy changes b4 commit.

--- securedeploy.sh
+++ securedeploy.sh
@@ -1,4 +1,10 @@
 #!bin/bash
+#Ask for the root password and set it to $root
 read -p "Set a root password: " root
+#set the admin sshkey
+key="`cat ~/.ssh/admin.pub`"
+#Create and boot a new linode in us-east, on a 2GB Plan, Ubuntu 18.04 loaded with my ssh key
+linode-cli linodes create --root_pass $root --region 'us-east' --type 'g6-standard-1' \
+--image 'linode/ubuntu18.04' --authorized-keys $key --booted true 
 
-linode-cli linodes create --root_pass $root 
+
