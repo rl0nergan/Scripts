db182f733e5f463da091d601fc1f0f09cfd0a955
Ryan Lonergan <Ryan.tlonergan@gmail.com>
2020-03-16 21:48:30 -0400

    deploy.sh will now prompt for a root password and username to configure ssh keys for root. Then it calls the linode-cli to create a new instance. It receives the output in json. Then it calls check_status.sh to check if it is running and ready for connections.

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 #Ask for the root password and set it to $root
 #Change to secret entry once complete
-read -p "Set a root password: " root
+read -s -p "Set a root password: " root
 #set the admin sshkey. Paste it in for now
-read -p "Enter SSH Key: " key
-key_list+=( $key )
+#key=`echo `
+read -p "Enter a username for SSH Key: " key
+key_list+=( $key  )
 #Create and boot a new linode in us-east, on a 2GB Plan, Ubuntu 18.04 loaded with my ssh key
 output=`linode-cli linodes create --root_pass $root --region 'us-east' --type 'g6-standard-1' \
---image 'linode/ubuntu18.04' --authorized_keys $key_list --booted true \
+--image 'linode/ubuntu18.04' --authorized_users $key_list --booted true \
 --label 'scripted' --json`
 
-echo $output[0]
+echo $output
 
-#running=false
-#while [ $running == false ]
-#  do
-#    linode-cli linodes
-#  done
-
+check_status.sh
 
 #[{
     "id": 19775744, 
