d9583d442cd4382da5d844f3284f486f18f5befa
Ryan Lonergan <Ryan.tlonergan@gmail.com>
2020-03-16 20:07:17 -0400

    Was able to ask for user input to set the root password and SSH key for the new linode to deploy. Next step will be to extract the LinodeID out of the response and use that to check for running status.

--- securedeploy.sh
+++ securedeploy.sh
@@ -1,10 +1,19 @@
-#!bin/bash
+#!/bin/bash
 #Ask for the root password and set it to $root
+#Change to secret entry once complete
 read -p "Set a root password: " root
 #set the admin sshkey
-key="`cat ~/.ssh/admin.pub`"
+read -p "Enter SSH Key: " key
+key_list+=( $key )
 #Create and boot a new linode in us-east, on a 2GB Plan, Ubuntu 18.04 loaded with my ssh key
-linode-cli linodes create --root_pass $root --region 'us-east' --type 'g6-standard-1' \
---image 'linode/ubuntu18.04' --authorized-keys $key --booted true 
+output=`linode-cli linodes create --root_pass $root --region 'us-east' --type 'g6-standard-1' \
+--image 'linode/ubuntu18.04' --authorized_keys $key_list --booted true \
+--label 'scripted' --json`
 
+echo $output
 
+#running=false
+#while [ $running == false ]
+#  do
+#    linode-cli linodes
+#  done
