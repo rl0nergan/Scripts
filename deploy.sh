00afe8a1f0cd40d3ae23e0dd90bbd618cab06a69
Ryan Lonergan <Ryan.tlonergan@gmail.com>
2020-03-16 20:29:18 -0400

    Breaking up script. deploy.sh is currently able to start up a new 2GB Linode with the user inputting a root password and an SSHKey to setup the new instance with and gets a json response with instance information.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 #Ask for the root password and set it to $root
 #Change to secret entry once complete
 read -p "Set a root password: " root
-#set the admin sshkey
+#set the admin sshkey. Paste it in for now
 read -p "Enter SSH Key: " key
 key_list+=( $key )
 #Create and boot a new linode in us-east, on a 2GB Plan, Ubuntu 18.04 loaded with my ssh key
@@ -10,10 +10,21 @@ output=`linode-cli linodes create --root_pass $root --region 'us-east' --type 'g
 --image 'linode/ubuntu18.04' --authorized_keys $key_list --booted true \
 --label 'scripted' --json`
 
-echo $output
+echo $output[0]
 
 #running=false
 #while [ $running == false ]
 #  do
 #    linode-cli linodes
 #  done
+
+
+#[{
+    "id": 19775744, 
+    "label": "scripted", 
+    "status": "provisioning", 
+    "type": "g6-standard-1", 
+    "ipv4": ["45.79.180.221"], 
+    "image": "linode/ubuntu18.04", 
+    "region": "us-east"
+    }]
